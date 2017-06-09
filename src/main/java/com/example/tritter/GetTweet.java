f5fe320b4e2838566e0adf9442f1a983cce10cba
AkitoOnishi <aki325natudi@gMail.com>
2017-06-09 17:36:53 +0900

    ツイート取得メソッド

---
+++ src/main/java/com/example/tritter/GetTweet.java
@@ -0,0 +1,22 @@
+package com.example.tritter;
+import twitter4j.*;
+
+import java.util.List;
+public class GetTweet {
+    public static void main(String[] args) {
+        try {
+            // gets Twitter instance with default credentials
+            Twitter twitter = new TwitterFactory().getInstance();
+            User user = twitter.verifyCredentials();
+            List<Status> statuses = twitter.getHomeTimeline();
+            System.out.println("Showing @" + user.getScreenName() + "'s home timeline.");
+            for (Status status : statuses) {
+                System.out.println("@" + status.getUser().getScreenName() + " - 「" + status.getText()+"」       ふぁぼ"+status.getFavoriteCount()+"     りつい"+status.getRetweetCount());
+            }
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to get timeline: " + te.getMessage());
+            System.exit(-1);
+        }
+    }
+}

b3d554534587b4609517a9fb4190b78b713237f3
AkitoOnishi <aki325natudi@gMail.com>
2017-06-12 17:15:59 +0900

    test:一つ目のツイート表示

--- src/main/java/com/example/tritter/GetTweet.java
+++ src/main/java/com/example/tritter/GetTweet.java
@@ -13,7 +13,9 @@ public class GetTweet {
             System.out.println("Showing @" + user.getScreenName() + "'s home timeline.");
             System.out.println("プロフィール画像のURL:"+user.getMiniProfileImageURL());
           //ついーとしてみる
-            twitter.updateStatus("s");
+           // twitter.updateStatus("st");
+            System.out.println("一つ目のツイートを表示: "+statuses.get(0).getText());
+            
             for (Status status : statuses) {
                 System.out.println("@" + status.getUser().getScreenName() + " - 「" + status.getText()+"」       ふぁぼ"+status.getFavoriteCount()+"     りつい"+status.getRetweetCount());
             }
